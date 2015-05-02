0760c58525c597b67aa678f2d318f9580c9fb1bb
renanvalimp <renanvalimp@gmail.com>
2015-05-02 13:28:04 -0300

    cadastro aluno 1.3

--- index.php
+++ index.php
@@ -31,7 +31,8 @@
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
-          <li class="active"><a href="index.php">Main Page</a></li>        
+          <li class="active"><a href="index.php">Main Page</a></li>
+          <li class="active"><a href="cadastroUsuario.php">Cadastrar Usuário</a></li>
       </ul>
 
     </div><!-- /.navbar-collapse -->
